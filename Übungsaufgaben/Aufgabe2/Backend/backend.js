37d4b1bf41705a31f8f9ad0de43402edb502fbf0
Niklas574 <Niklas.Dogan@plan-b-gmbh.com>
2024-03-22 08:49:04 +0100

    backend.js hinzugefügt

---
+++ Übungsaufgaben/Aufgabe2/Backend/backend.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var btnElement = document.querySelector('.btn');
+
+    btnElement.addEventListener('click', function() {
+        alert('Der Button wurde geklickt!');
+    });
+};
